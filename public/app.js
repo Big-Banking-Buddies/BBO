f1158a03e7a3a5b0e2ed436fce03ca4e506004a6
ronnie-kauanoe <31224445+ronnie-kauanoe@users.noreply.github.com>
2021-02-12 11:45:20 -1000

    Initial Firebase commit

---
+++ public/app.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", event => {
+    const app = firebase.app();
+    const db = firebase.firestore();
+    const myPost = db.collection('posts').doc('firstpost');
+
+    myPost.onSnapshot(doc => {
+        const data = doc.data();
+    });
+
+});
+
+function googleLogin() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(console.log);
+}
+
+function updatePost(e) {
+    const db = firebase.firestore();
+    const myPost = db.collection('posts').doc('firstpost');
+    myPost.update({ title: e.target.value });
+}
