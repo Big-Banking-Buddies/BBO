478bc39bf3018e65d910d9be86e807c38c0112fc
nicholasmp <nkmp@hawaii.edu>
2021-03-16 18:14:17 -1000

    Added Footer Styling change.
    Added a potential change to the footer, new is above the original.

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Grid, Icon, Label } from 'semantic-ui-react';
 
 function Footer() {
   const divStyle = { paddingTop: '15px', paddingBottom: '15px' };
   return (
       <footer style={{ background: 'black', color: 'white' }}>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={6}>
+              <h2>Main</h2>
+              <Label>About</Label>
+              <Label>FAQ</Label>
+            </Grid.Column>
+            <Grid.Column width={6}>
+              <h2>Links</h2>
+              <Label>FAQ</Label>
+              <Label>Privacy Policy</Label>
+            </Grid.Column>
+            <Grid.Column width={6}>
+              <h2>Contact Info</h2>
+              <Icon name='twitter'/>
+              <Icon name='facebook' />
+              <Icon name='instagram' />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
         <div style={divStyle} className="ui center aligned container">
           BBO is a student project under the University of Hawaii at Manoa Spring 2021. <br/>
           This project aims to explore cyber security techniques, and as such this website shouldn't <br/>
