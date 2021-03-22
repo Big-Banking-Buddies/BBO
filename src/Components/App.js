5a98edbb306acbaadc3eeef2e32f2bcd1b327b3f
Yuhan Jiang <jiangyuh@hawaii.edu>
2021-03-21 14:08:25 -1000

    bootstrap

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,13 +1,14 @@
-import NavBar from './NavBar';
-import Footer from './Footer';
+import NavBar from '../NavBar';
+import Footer from '../Footer';
 import 'semantic-ui-css/semantic.min.css';
-import LandingPage from './LandingPage';
+import LandingPage from '../LandingPage';
+import Signup from './Signup';
 
 function App() {
   return (
       <>
         <NavBar/>
-        <LandingPage/>
+        <Signup/>
         <Footer/>
       </>
   );
---
+++ src/Components/Signup.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Form, Button, Card, Alert } from "react-bootstrap"
+
+export default function Signup() {
+  <>
+    <Card>
+      <Card.Body>
+        <h2 className="text-center mb-4">Sign Up</h2>
+      </Card.Body>
+    </Card>
+    <div className="w-100 text-center mt-2">
+      Already have an account? Log In
+    </div>
+  </>
+}
