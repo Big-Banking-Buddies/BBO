91b26f7d50de642b3e09d2a7e88786c1c3dc05f0
Yuhan Jiang <jiangyuh@hawaii.edu>
2021-04-09 12:01:30 -1000

    Update PrivateRoute.js

--- src/Components/PrivateRoute.js
+++ src/Components/PrivateRoute.js
@@ -5,12 +5,12 @@ import { useAuth } from '../Context/AuthContext'
 
 export default function PrivateRoute({ component: Component, ...rest }) {
   const { currentUser } = useAuth()
-
+  // if there is no current user, redirect to the landing page
   return (
       <Route
           {...rest}
           render={props => {
-            return currentUser ? <Component {...props} /> : <Redirect to="/login" />
+            return currentUser ? <Component {...props} /> : <Redirect to="/landing" />
           }}
       ></Route>
   )
