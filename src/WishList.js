980dd5315d262820666b7eff89343e056e0bfca3
nicholasmp <nkmp@hawaii.edu>
2021-05-01 23:45:01 -1000

    Added a database function for the Wish List page
    Wish List page has an added database feature that requres a 'npm run database' call to function.

--- src/WishList.js
+++ src/WishList.js
@@ -1,30 +1,62 @@
 // Majority of the code is based on the to do list from Traversy Media https://www.youtube.com/watch?v=w7ejDZ8SWv8&t=5798s
-import React, { useState } from 'react';
-import { Grid, Card, List, Form, Input, Button } from 'semantic-ui-react';
+import React, { useState, useEffect } from 'react';
+import { Grid, Card, List, Form, Input, Button, Icon } from 'semantic-ui-react';
+import { useAuth } from './Context/AuthContext.js';
 
 function WishList () {
-  const [items, setItems ] = useState([
-    { id: 1,
-      text: 'Shirt',
-      website: 'https://www.amazon.com/Carhartt-Workwear-T-Shirt-Original-X-Large/dp/B0007XB2YI/ref=sr_1_2?dchild=1&keywords=shirt&qid=1617965318&sr=8-2', },
-    { id: 2,
-      text: 'Shorts',
-      website: 'https://www.amazon.com/Under-Armour-Shorts-Venom-Black/dp/B0872N2JVZ/ref=sr_1_4_sspa?dchild=1&keywords=mens+shorts&qid=1617966794&sr=8-4-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFXQlAxM1dLRFNYUiZlbmNyeXB0ZWRJZD1BMDk0MDI0NDJOWUcxT1NTNFVUNzQmZW5jcnlwdGVkQWRJZD1BMDMwMTI0MTFRMVgySTlOUElVV0smd2lkZ2V0TmFtZT1zcF9hdGYmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl', },
-  ]);
+  const { currentUser } = useAuth();
+  const [items, setItems ] = useState([]);
+  const [listIndex, setListIndex] = useState(0);
+  useEffect(() => {
+    const getItems = async () => {
+      const dbItems = await fetchWishList();
+      setListIndex(dbItems.length);
+      console.log(listIndex);
+      const dbWishList = [];
+      dbItems.forEach(function(dbItem) {
+        if (currentUser.email === dbItem.user) {
+          dbWishList.push(dbItem);
+        }
+      });
+      setItems(dbWishList);
+    }
+    getItems();
+  }, [currentUser.email, listIndex]);
+  const fetchWishList = async () => {
+    const res = await fetch('http://localhost:5000/items');
+    return await res.json();
+  }
 
   const [itemText, setItemText] = useState('');
   const [webLink, setWebLink] = useState('');
 
   const handleSubmit = () => {
-    let newId = items.length + 1;
+    let newId = listIndex + 1;
+    if (!webLink.includes('www.amazon.com')) {
+      window.alert('Input Link was not an Amazon Link and might be unsafe.');
+      return;
+    }
     if(!webLink.startsWith('https://', 0)) {
       let site = 'https://' + webLink;
-      let newItem = { id: newId, text: itemText, website: site };
-      setItems([...items, newItem]);
+      let newItem = { id: newId, user: currentUser.email, text: itemText, website: site };
+      handleAdd(newItem);
     } else {
-      let newItem = { id: newId, text: itemText, website: webLink };
-      setItems([...items, newItem]);
+      let newItem = { id: newId, user: currentUser.email, text: itemText, website: webLink };
+      handleAdd(newItem);
     }
+  }
+  const handleAdd = async (item) => {
+    const res = await fetch('http://localhost:5000/items/', {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json' },
+      body: JSON.stringify(item),
+    });
+    const newItem = await res.json();
+    setItems([...items, newItem]);
+  }
+  const handleDelete = async (id) => {
+    await fetch(`http://localhost:5000/items/${id}`, {method: 'DELETE'});
+    setItems(items.filter((item) => item.id !== id));
   }
 
   return (
@@ -50,15 +82,25 @@ function WishList () {
                       <Button type='submit' onClick={() => handleSubmit()}>Submit</Button>
                     </Form.Group>
                   </Form>
-
-                  <List bulleted divided relaxed >
-                    {items.map((item) => (
+                </Card.Content>
+                <Card.Content textAlign={'center'}>
+                  <Card.Header>Wish List Items</Card.Header>
+                </Card.Content>
+                <Card.Content>
+                  <List divided relaxed >
+                    {items.length > 0 ? (items.map((item) => (
                         <List.Item key={item.id}>
                           <List.Content>
-                            <List.Header href={item.website}>{item.text}</List.Header>
+                            <List.Header>
+                              <a href={item.website}>{item.text}</a>
+                              <Button icon floated='right' labelPosition='right' onClick={() => handleDelete(item.id)} size='medium'>
+                                <Icon name='delete'/>
+                                Delete Item
+                              </Button>
+                            </List.Header>
                           </List.Content>
                         </List.Item>
-                    ))}
+                    ))) : ('No Wish List Items Found') }
                   </List>
                 </Card.Content>
               </Card>
